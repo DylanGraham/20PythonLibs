7f407a0ca7226892eab6613ed2d3069b7095f545
Dylan Graham <dylan.graham@gmail.com>
2016-12-06 11:37:06 +1100

    default dict

--- 20PythonLibs.py
+++ 20PythonLibs.py
@@ -1,9 +1,12 @@
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 
 ascii_lowercase = ['a', 'b', 'c', 'd', 'e']
 
-di = dict(zip(ascii_lowercase, range(5)))
+dict01 = dict(zip(ascii_lowercase, range(5)))
 od = OrderedDict(zip(ascii_lowercase, range(5)))
 
-print(di)
+print(dict01)
 print(od)
+
+dd = defaultdict(list)
+print(dd['a'])
